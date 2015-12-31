4beea8edf844854ae87a8ab5c9c062a7b1487a2b
Dan Whitman <dwhitman44@gmail.com>
2015-12-30 21:54:08 -0500

    -Enable multitap (multiplayer5) if a buton is pressed on a controller other than the first two.

--- source/ppu.c
+++ source/ppu.c
@@ -2361,12 +2361,6 @@ static void CommonPPUReset()
    S9xFixColourBrightness();
    IPPU.PreviousLine = IPPU.CurrentLine = 0;
 
-   if (Settings.ControllerOption == 0)
-      IPPU.Controller = SNES_MAX_CONTROLLER_OPTIONS - 1;
-   else
-      IPPU.Controller = Settings.ControllerOption - 1;
-   S9xNextController();
-
    for (c = 0; c < 2; c++)
       memset(&IPPU.Clip [c], 0, sizeof(struct ClipData));
 
@@ -2542,7 +2536,9 @@ void ProcessSuperScope()
    }
 }
 
-void S9xNextController()
+// DJW: Not what why this function exists given that
+// it's only called once unecessarily
+/*void S9xNextController()
 {
    switch (IPPU.Controller)
    {
@@ -2590,6 +2586,7 @@ void S9xNextController()
       break;
    }
 }
+*/
 
 void S9xUpdateJustifiers()
 {
